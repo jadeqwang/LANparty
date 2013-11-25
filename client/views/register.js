9733a5538efb6d9deed5d4f3031c3ddd527c39b7
Jade <jade@meteor.com>
2013-11-25 02:58:32 -0800

    user's name shown in registration

--- client/views/register.js
+++ client/views/register.js
@@ -6,3 +6,11 @@ Template.register.events({
 		var $comments = $(e.target).find(['name=comments']);
 	}
 })
+
+Template.register.helpers({
+	yourname: function() {
+		var user = Meteor.user();
+		console.log(user.profile.name);
+		return user.profile.name;
+	}
+})
